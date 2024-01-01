653861f4f4e5b3a708e7494829a0216d6be3f582
hitorigods <hitorigods1124@gmail.com>
2024-01-01 16:30:50 +0900

    prisma:seedの仮データ用意

--- prisma/migrations/20240101072048_init/migration.sql
+++ prisma/migrations/20240101072048_init/migration.sql
@@ -1,10 +1,10 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "email" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "name" VARCHAR(255) NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -12,35 +12,38 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Profile" (
-    "id" TEXT NOT NULL,
-    "bio" TEXT,
-    "user_id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "avaterUrl" TEXT,
+    "displayName" VARCHAR(255),
+    "user_id" UUID NOT NULL,
 
     CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Message" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "title" VARCHAR(255) NOT NULL,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "user_id" TEXT NOT NULL,
-    "room_id" TEXT NOT NULL,
-
-    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Room" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "title" VARCHAR(255) NOT NULL,
     "published" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "user_id" UUID NOT NULL,
+    "room_id" UUID NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -48,12 +51,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Profile_user_id_key" ON "Profile"("user_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Message_user_id_key" ON "Message"("user_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Message_room_id_key" ON "Message"("room_id");
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,180 @@
 import { Prisma, PrismaClient } from '@prisma/client';
-import { hashSync } from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
-async function main() {
-	await prisma.user.deleteMany();
-
+/**
+ * Userテーブルの初期データを作成する
+ */
+async function usersCreate() {
 	const users: Prisma.UserCreateInput[] = [
 		{
 			email: 'test01@test.com',
 			name: 'testuser01',
-			// password: hashSync('test01', 8),
+			password: bcrypt.hashSync('test01', 8),
 		},
 		{
 			email: 'test02@test.com',
 			name: 'testuser02',
-			// password: hashSync('test02', 8),
+			password: bcrypt.hashSync('test02', 8),
 		},
 		{
 			email: 'test03@test.com',
 			name: 'testuser03',
-			// password: hashSync('test03', 8),
+			password: bcrypt.hashSync('test03', 8),
 		},
 	];
 
+	const createdUsers = [];
 	for (const user of users) {
-		await prisma.user.create({
-			data: user,
+		createdUsers.push(await prisma.user.create({ data: user }));
+	}
+	return createdUsers;
+}
+
+/**
+ * Profileテーブルの初期データを作成する
+ */
+async function profileCreate() {
+	const profiles: Prisma.ProfileCreateInput[] = [
+		{
+			avaterUrl: 'http://localhost:3000/avater/avater01.png',
+			displayName: 'Test User 01',
+			user: {
+				connect: { email: 'test01@test.com' },
+			},
+		},
+		{
+			avaterUrl: 'http://localhost:3000/avater/avater02.png',
+			displayName: 'Test User 02',
+			user: {
+				connect: { email: 'test02@test.com' },
+			},
+		},
+		{
+			avaterUrl: 'http://localhost:3000/avater/avater03.png',
+			displayName: 'Test User 03',
+			user: {
+				connect: { email: 'test03@test.com' },
+			},
+		},
+	];
+
+	for (const profile of profiles) {
+		await prisma.profile.create({
+			data: profile,
 		});
+	}
+}
+
+/**
+ * Roomテーブルの初期データを作成する
+ */
+async function roomCreate() {
+	const rooms: Prisma.RoomCreateInput[] = [
+		{
+			title: 'Test Room 01',
+			published: true,
+		},
+		{
+			title: 'Test Room 02',
+			published: false,
+		},
+		{
+			title: 'Test Room 03',
+			published: true,
+		},
+	];
+
+	const createdRooms = [];
+	for (const room of rooms) {
+		createdRooms.push(await prisma.room.create({ data: room }));
+	}
+	return createdRooms;
+}
+
+/**
+ * Messageテーブルの初期データを作成する
+ */
+async function messageCreate(userIds: string[], roomIds: string[]) {
+	const messages: Prisma.MessageCreateInput[] = [
+		{
+			title: 'Test Message 01',
+			published: true,
+			user: {
+				connect: { id: userIds[0] },
+			},
+			room: {
+				connect: { id: roomIds[0] },
+			},
+		},
+		{
+			title: 'Test Message 02',
+			published: true,
+			user: {
+				connect: { id: userIds[1] },
+			},
+			room: {
+				connect: { id: roomIds[0] },
+			},
+		},
+		{
+			title: 'Test Message 03',
+			published: true,
+			user: {
+				connect: { id: userIds[2] },
+			},
+			room: {
+				connect: { id: roomIds[0] },
+			},
+		},
+		{
+			title: 'Test Message 04',
+			published: true,
+			user: {
+				connect: { id: userIds[0] },
+			},
+			room: {
+				connect: { id: roomIds[1] },
+			},
+		},
+		{
+			title: 'Test Message 05',
+			published: true,
+			user: {
+				connect: { id: userIds[0] },
+			},
+			room: {
+				connect: { id: roomIds[1] },
+			},
+		},
+	];
+
+	for (const message of messages) {
+		await prisma.message.create({
+			data: message,
+		});
+	}
+}
+
+/**
+ * メイン処理
+ */
+async function main() {
+	await prisma.message.deleteMany();
+	await prisma.room.deleteMany();
+	await prisma.profile.deleteMany();
+	await prisma.user.deleteMany();
+
+	const users = await usersCreate();
+	await profileCreate();
+	const rooms = await roomCreate();
+	if (rooms) {
+		const user_ids = users.map((user) => user.id);
+		const room_ids = rooms.map((room) => room.id);
+		await messageCreate(user_ids, room_ids);
+	} else {
+		console.error('Room creation failed');
 	}
 }
 
