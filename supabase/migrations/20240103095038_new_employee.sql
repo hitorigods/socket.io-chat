08008d54676f0356699b44a484dd84b739c83249
hitorigods <hitorigods1124@gmail.com>
2024-01-04 01:58:41 +0900

    updatedAtを自動更新するようにSQL設定

--- supabase/migrations/20240103095038_new_employee.sql
+++ supabase/migrations/20240103095038_new_employee.sql
@@ -2,8 +2,8 @@ alter database postgres set timezone to 'Asia/Tokyo';
 
 create extension if not exists moddatetime schema extensions;
 
-create trigger handle_user_updated_at before update on user
+create trigger handle_user_updated_at before update on public.user
   for each row execute procedure moddatetime ("updatedAt");
 
-create trigger handle_chat_updated_at before update on chat
+create trigger handle_chat_updated_at before update on public.chat
   for each row execute procedure moddatetime ("updatedAt");
