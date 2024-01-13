a65b454bcab30e43ca65426a6adea7786e85f831
hitorigods <hitorigods1124@gmail.com>
2024-01-13 18:14:11 +0900

    デプロイ用OAuth調整2

--- src/utils/libs/supabase.ts
+++ src/utils/libs/supabase.ts
@@ -2,6 +2,15 @@ import { createClient } from '@supabase/supabase-js';
 
 import { Database } from '@/utils/libs/supabase.types';
 
+console.log(
+	'process.env.NEXT_PUBLIC_SUPABASE_URL',
+	process.env.NEXT_PUBLIC_SUPABASE_URL
+);
+console.log(
+	'process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY',
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
